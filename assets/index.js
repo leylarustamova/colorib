773081051b65b42eae4f7a68994f19e247e199f3
leylarustamova <leylatustamova46@gmail.com>
2025-04-29 00:25:47 +0400

    added file

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll(".btn");
 const items = document.querySelectorAll(".item");
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
@@ -18,3 +20,21 @@ buttons.forEach(button => {
     });
   });
 });
+
+
+
+
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
